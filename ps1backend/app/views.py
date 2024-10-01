cbb12ac4b531b4e426aeca27854b0777d3d7a4c9
Josh Martin <joshmartin@reprogrammed.software>
2024-10-01 06:00:00 -0500

    Draft of home page

--- ps1backend/app/views.py
+++ ps1backend/app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import generic
+
 
 # Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'app/index.html'
+    # context_object_name = 'items'
+    # login_url = '/hello'
+    # redirect_field_name = 'redirect_to'
+
+    def get_queryset(self):
+        return []
+
+    # def get_context_data(self, *, object_list=None, **kwargs):
+    #     context = super(IndexView, self).get_context_data(**kwargs)
+    #     context['is_superuser'] = self.request.user.is_superuser
+    #     context['current_site'] = Site.objects.get_current()
+    #     return context
